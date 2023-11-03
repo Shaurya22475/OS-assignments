3e4801d3d0d7bdce0d7c51a534ed8384eb38c4e3
Shaurya22475 <132266046+Shaurya22475@users.noreply.github.com>
2023-11-04 01:19:10 +0530

    Update smartloader.c

--- smart/smartloader.c
+++ smart/smartloader.c
@@ -3,7 +3,8 @@
 Elf32_Ehdr ehdr;
 Elf32_Phdr phdr;
 int fd;
-
+int page_faults=0;
+int total_internal_segmentation;
 /*
  * release memory and other cleanups
  */
@@ -16,8 +17,10 @@ void loader_cleanup() {
  */
 
 void sigsegv_handler(int signo, siginfo_t *info, void *context){
+
     if (signo == SIGSEGV){
         Elf32_Addr faulting_address = (Elf32_Addr)info->si_addr;
+        page_faults++;
         for (int i = 0; i < ehdr.e_phnum; i++) {
         if (read(fd, &phdr, sizeof(Elf32_Phdr)) != sizeof(Elf32_Phdr)) {
             perror("read");
@@ -25,22 +28,24 @@ void sigsegv_handler(int signo, siginfo_t *info, void *context){
             close(fd);
             exit(1);
         }
-        printf("Counter:: %d %d\n",i,phdr.p_type);
+        int internal_segmentation=0;
         if (faulting_address >= phdr.p_vaddr && faulting_address <= phdr.p_vaddr + phdr.p_memsz){
             printf("Faulting address: %u, segment address: %u %u\n",faulting_address,phdr.p_vaddr,phdr.p_vaddr+phdr.p_memsz);
-
-            size_t required_size = phdr.p_memsz;
             size_t page_size = 4096;
-            size_t allocation_size = ((required_size + page_size - 1) / page_size) * page_size;
-            void* segment_address = mmap((void * )phdr.p_vaddr, allocation_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE, fd, phdr.p_offset);
-            printf("Segment address allocated  : %u",(int)segment_address);
+            void* segment_address = mmap((void * )faulting_address, page_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE|MAP_FIXED, fd, phdr.p_offset);
+            if (phdr.p_vaddr+phdr.p_memsz>= faulting_address+page_size){
+                internal_segmentation=0;
+            }
+            else{
+                internal_segmentation=(int)(faulting_address+page_size)-((int)(phdr.p_vaddr+phdr.p_memsz));
+            }
              if (segment_address == MAP_FAILED) {
                     perror("mmap");
                     printf("hey\n");
                     close(fd);
                     exit(1);
                 }
-            printf("Alloc done;;\n");
+            total_internal_segmentation+=internal_segmentation;
             break;
         }
       }
@@ -48,6 +53,7 @@ void sigsegv_handler(int signo, siginfo_t *info, void *context){
       printf("EXITINIG OUT AFTER ALLOCATING:\n");
    } 
 }
+
 void load_and_run_elf(char** exe) {
      
     struct sigaction sa;
@@ -65,28 +71,14 @@ void load_and_run_elf(char** exe) {
     read(fd, &ehdr, sizeof(Elf32_Ehdr));
 
     unsigned int program_header_offset = ehdr.e_phoff;
-    unsigned int curr= lseek(fd,0,SEEK_CUR);
 
     Elf32_Addr entry_point = ehdr.e_entry;
     printf("Number of program headers: %d",ehdr.e_phnum);
     int (*_start)()=(int (*)())(entry_point);
     printf("seg..%u",phdr.p_vaddr);
     int result = _start();
-    // void* segment_address = mmap(NULL, phdr.p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE, fd, phdr.p_offset);
-    // if (segment_address == MAP_FAILED) {
-    //     perror("mmap");
-    //     close(fd);
-    //     exit(1);  
-    // }
-
     printf("User _start return value = %d\n", result);
-
-        // if (munmap(segment_address, phdr.p_memsz) == -1) {
-        //     perror("munmap");
-        //}
-
     close(fd);
-    
 }
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -95,6 +87,9 @@ int main(int argc, char** argv) {
     }
     printf("%d",getpid());
     load_and_run_elf(argv);
+    printf("Total number of page faults: %d\n",page_faults);
+    printf("Total number of page allocations carried out : %d\n",page_faults);
+    printf("Total internal segmentation: %f KB\n",total_internal_segmentation/1024.0);
     loader_cleanup();
     return 0;
 }
